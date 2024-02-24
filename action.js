a7a41740e4eb16cb75c2a4f15a9b5261fe110d19
hedie <rahimihedie22@gmail.com>
2024-02-24 18:11:09 +0330

    work on API and data

--- action.js
+++ action.js
@@ -25,21 +25,40 @@ document.addEventListener("DOMContentLoaded", () => {
   let showDateTime = document.querySelector(".time");
   showDateTime.innerHTML = `${day} ${hours}:${minutes} `;
 
-  function showCity(event) {
-    event.preventDefault();
-    let city = document.querySelector(".input").value;
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=a49f0cad903e09dc8e1t8o40aab88ab3&units=metric`;
+  function showCity(city) {
+    let apiKey = "a49f0cad903e09dc8e1t8o40aab88ab3";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
 
-    axios.get(apiUrl).then(showTemperature);
+    axios.get(apiUrl).then(showDetails);
   }
-  let cityShow = document.querySelector(".search-input");
-  cityShow.addEventListener("submit", showCity);
 
-  function showTemperature(response) {
+  function submitCity(event) {
+    event, preventDefault();
+    let searchInput = document.querySelector(".search-input");
+
+    showCity(searchInput.value);
+  }
+  citySearchForm = document.querySelector(".search-input");
+  citySearchForm.addEventListener("submit", showCity);
+
+  function showDetails(response) {
     let city = (document.querySelector(".city").innerHTML = response.data.city);
     let input = document.querySelector(".input");
 
     let cityTemp = (document.querySelector(".temperature").innerHTML =
       Math.round(response.data.temperature.current));
+
+    let description = (document.querySelector(".description").innerHTML =
+      response.data.condition.description);
+    let humidity = (document.querySelector(".humidity").innerHTML = Math.round(
+      response.data.temperature.humidity
+    ));
+    let wind = (document.querySelector(".wind").innerHTML = Math.round(
+      response.data.wind.speed
+    ));
+    let mainIcon = document.querySelector(".main-icon");
+    mainIcon.setAttribute("src", response.data.condition.icon_url);
   }
+
+  showCity("london");
 });
